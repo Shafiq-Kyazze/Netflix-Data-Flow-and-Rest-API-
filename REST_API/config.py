8d378b4d85ac9523763cbce7009d75bf2393a560
Shafiq-Kyazze <58377262+Shafiq-Kyazze@users.noreply.github.com>
2021-07-28 22:26:43 +0100

    Changing config file

--- REST_API/config.py
+++ REST_API/config.py
@@ -1,3 +1,3 @@
 """Config file"""
 
-DATABASE_URI = "postgresql://ddybjwna:WnijU9fBXyHsW3x-0AhGSP4PUWtCAQFs@rogue.db.elephantsql.com/ddybjwna"
+DATABASE_URI = "postgresql://****:**-***@rogue.db.elephantsql.com/**"
