0ad1f801bbeb4f4838b0f4f30998fe0e65fbd09d
Shafiq-Kyazze <shafiqkyazze@live.com>
2021-08-14 23:56:11 +0100

    changed the run file host

--- REST_API/run.py
+++ REST_API/run.py
@@ -3,4 +3,4 @@ from main.init import _init_app
 app = _init_app()
 
 if __name__ == '__main__':
-    app.run(port=5000)    #Port set to 5000 since app is in development
+    app.run(host='0.0.0.0')    #Port set to 5000 since app is in development
