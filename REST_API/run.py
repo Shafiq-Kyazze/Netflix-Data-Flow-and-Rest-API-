9f549a29b8c6a2e5d346235e4f57de6cca8e6dd0
Shafiq-Kyazze <shafiqkyazze@live.com>
2021-08-15 01:13:04 +0100

    changed the run file app.run to make sure the api is run from the docker containers host

--- REST_API/run.py
+++ REST_API/run.py
@@ -3,4 +3,4 @@ from main.init import _init_app
 app = _init_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')    #Port set to 5000 since app is in development
+    app.run(host='0.0.0.0', port=5000, debug=True)    #Port set to 5000 since app is in development
